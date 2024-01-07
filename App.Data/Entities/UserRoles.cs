5b2778a5f1cb3bc446455571005309bd764e1e29
furkanucgull <furkanucgull@gmail.com>
2024-01-07 16:42:51 +0100

    UserRolesEntityandContactMsgEntity

---
+++ App.Data/Entities/UserRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entities
+{
+    public enum UserRoles
+    {
+        Admin,
+        Moderator,
+        BasicUser,
+        StandartUser,
+        PremiumUser,
+    }
+}
